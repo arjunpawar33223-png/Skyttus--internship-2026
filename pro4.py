0a5540426c0271a1ab0773f0748bbd5becf2170d
arjunpawar33223-png <arjunpawar33223@gmail.com>
2026-01-19 15:20:56 +0530

    Add files via upload

--- pro4.py
+++ pro4.py
@@ -1,3 +1,8 @@
-s = input("Enter a string: ")
-print("First:", s[0])
-print("Last:", s[-1])
+n = int(input("Enter number of terms: "))
+
+a = 0
+b = 1
+
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
